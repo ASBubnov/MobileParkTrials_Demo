5820f5ff026991b9701eb2461e0f46c65442fa97
Alexander Bubnov <alesbubnov@gmail.com>
2023-09-21 17:03:22 +0300

    Изменил алгоритм поиска слова с наибольшим кол-ом гласных

--- NewsLibrary/Model/News.cs
+++ NewsLibrary/Model/News.cs
@@ -20,8 +20,16 @@ namespace NewsLibrary.Model
             var r = new Regex("(a|A|e|E|i|I|o|O|u|U|y|Y|А|а|Я|я|У|у|Ю|ю|О|о|Е|е|Ё|ё|Э|э|И|и|Ы|ы)");
             char[] separators = new char[] { ' ', '.', ',', ':', ';', '?', '!', '>', '<', '$', '&', '%', '#', '@', '№', '*', '(', ')', '/', '\\', '\"', '+', '=','\r','\t' };
             var words = Description.Split(separators);
-            var mostVowels = words.Max(y => r.Matches(y).Count);
-            MostVowels = words.First(x => r.Matches(x).Count == mostVowels);
+            int counter = 0;
+            foreach (var word in words)
+            {
+                var currentCounter = r.Matches(word).Count;
+                if(currentCounter > counter)
+                {
+                    counter = currentCounter;
+                    MostVowels = word;
+                }
+            }
         }
     }
 }
